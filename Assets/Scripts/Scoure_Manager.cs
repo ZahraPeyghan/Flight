9f3c0374a60f2d9f90ae51d5db6751793d1356f1
Zahra Peyghan <82312745+ZahraPeyghan@users.noreply.github.com>
2023-01-15 22:30:51 +0330

    Defining the last score and showing it in the console section.

--- Assets/Scripts/Scoure_Manager.cs
+++ Assets/Scripts/Scoure_Manager.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Scoure_Manager : MonoBehaviour
 {
     public int Score = 0;
     public Text ScoreText;
     public int highScore = 0;
+    public static int lastScore = 0;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(score());
+        StartCoroutine(Reload());
         highScore = PlayerPrefs.GetInt("high_score" , 0);
         Debug.Log("High Score Stored :" + highScore);
+        Debug.Log("Last Score :" + lastScore);
     }
 
     // Update is called once per frame
@@ -29,6 +33,11 @@ public class Scoure_Manager : MonoBehaviour
         while(true){
              yield return new WaitForSeconds(2);
             Score = Score + 1;
+            lastScore = Score;
         }
+    }
+    IEnumerator Reload(){
+             yield return new WaitForSeconds(30);
+             SceneManager.LoadScene("Game");
     }
 }
