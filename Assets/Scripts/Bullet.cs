df3af6d7c03c7e0ba679fb500ff1aa9093054bcf
Zahra Peyghan <82312745+ZahraPeyghan@users.noreply.github.com>
2023-01-20 14:45:56 +0330

    Adding sound to the game environment while running and completing forward fring.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,11 @@ using UnityEngine.InputSystem;
 
 public class Bullet : MonoBehaviour
 {
+    public float Speed=10f;
+      void Update()
+    {
+       transform.Translate(Vector2.up * Speed * Time.deltaTime);
+    }
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.gameObject.tag == "Planes"){
             Destroy(gameObject);
